c276106d2a2dffe4f09eeb2d00a1b3a400d146e8
Yaroslav <yaraslav.ios.developer@gmail.com>
2023-10-28 01:21:31 +0200

    Initial Commit

---
+++ VPNTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VPNTestTask
+//
+//  Created by Yaroslav on 28/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
